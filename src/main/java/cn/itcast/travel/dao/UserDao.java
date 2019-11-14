2b6533a1bb694e9482b0cb1fd060004fcaa4fd6b
kraken <https://github.com/KrakentoNancy/travell.git>
2019-11-14 19:29:54 +0800

    测试上传项目

---
+++ src/main/java/cn/itcast/travel/dao/UserDao.java
@@ -0,0 +1,27 @@
+package cn.itcast.travel.dao;
+
+import cn.itcast.travel.domain.User;
+
+/**
+ * @author : kraken
+ * @date : 8:58 2019/11/12
+ */
+public interface UserDao {
+
+    /**
+     * 根据用户名查询用户信息
+     * @param username
+     * @return
+     */
+    public User findByUsername(String username);
+
+    /**
+     * 用户保存
+     * @param user
+     */
+    public void save(User user);
+
+    User findByCode(String code);
+
+    void updateStatus(User user);
+}
---
+++ src/main/java/cn/itcast/travel/service/UserService.java
@@ -0,0 +1,18 @@
+package cn.itcast.travel.service;
+
+import cn.itcast.travel.domain.User;
+
+/**
+ * @author : kraken
+ * @date : 8:57 2019/11/12
+ */
+public interface UserService {
+    /**
+     * 注册用户
+     * @param user
+     * @return
+     */
+    boolean regist(User user);
+
+    boolean active(String code);
+}
